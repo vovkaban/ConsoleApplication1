3728b1559d9291797766b25fc24d2ed70c05bde2
S-01 <S-01@TEACHER>
2021-09-29 12:43:20 +0300

    фффф

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -2,11 +2,25 @@
 //
 
 #include <iostream>
+#include <math.h>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!\n";
+    double q, w, e, S, P, a, b, c, a1, b1, c1;
+  
+    cin >> a >> b >> c >> a1 >> b1 >> c1;
+    q = sqrt((b - a) * (b - a) + (b1 - a1) * (b1 - a1));
+    w = sqrt((c - b) * (c - b) + (c1 - b1) * (c1 - b1));
+    e = sqrt((c - a) * (c - a) + (c1 - b1) * (c1 -b1));
+
+    P = (q + w + e);
+    S = sqrt((P / 2) * ((P / 2) - q) * ((P / 2) - w) * ((P / 2) - e));
+
+
+    cout << "Периметр треугольника = " <<P;
+    cout << "\n";
+    cout << "Площадь треугольника" << S;
 }
 
